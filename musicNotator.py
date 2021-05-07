97ef19729895a55e1e7a6ab359d3b0a8da290206
Enrique Mendez <eqm@mit.edu>
2021-05-07 17:05:16 -0400

    Seperate Window into Thirds.

--- musicNotator.py
+++ musicNotator.py
@@ -10,6 +10,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QHBoxLayout
 
 app = QApplication(sys.argv)
 
@@ -17,6 +18,23 @@ window = QWidget()
 window.setWindowTitle('PyQt5 App')
 window.setGeometry(100,100,280,80)
 window.move(60,15)
+
+#********
+# Create GUI
+#***************
+
+#seperate window into thirds
+layout	= QHBoxLayout()
+
+left  	= QWidget(parent=window)
+center	= QWidget(parent=window)
+right 	= QWidget(parent=window)
+
+layout.addWidget(left)
+layout.addWidget(center)
+layout.addWidget(right)
+
+window.setLayout(layout)
 
 hello_message = QLabel('<h1>Hello World</h1>', parent=window)
 hello_message.move(60,15)
